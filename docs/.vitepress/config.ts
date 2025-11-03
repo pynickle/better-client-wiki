e1ad04976475d61eb8be18012ac6d2ac7a2bebb5
pynickle <2330458484@qq.com>
2025-11-03 23:13:52 +0800

    fix: fix the wrong base url

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
+  base: '/better-client-wiki/',
   head: [['link', { rel: 'icon', href: '/better-client.png' }]],
   locales: {
     root: {
